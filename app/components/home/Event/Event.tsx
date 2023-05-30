3f06fd1eb8d9e51d40e47b8ec3fdb92586122927
mtake986 <tosa9629@gmail.com>
2023-05-30 17:33:22 -0600

    leftDays

--- app/components/home/Event/Event.tsx
+++ app/components/home/Event/Event.tsx
@@ -1,15 +1,14 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { builtInEvents } from "@/public/CONSTANTS";
 import { AiOutlineInfoCircle } from "react-icons/ai";
 const Event = () => {
   const [showInfo, setShowInfo] = useState<boolean>(false);
-  const [leftDays, setLeftDays] = useState<number>(999);
+  const [leftDays, setLeftDays] = useState<number>(-1);
+  const [loading, setLoading] = useState<boolean>(true);
   const toggleInfo = () => {
     setShowInfo(!showInfo);
-    if (showInfo) {
-      setTimeout(() => setShowInfo(!showInfo), 3000);
-    }
+    setTimeout(() => setShowInfo(false), 3000);
   };
 
   const calculateLeftDays = () => {
@@ -19,17 +18,22 @@ const Event = () => {
       builtInEvents[0].date.month - 1,
       builtInEvents[0].date.day
     );
-    const diffTime = Math.abs(eventDate.getTime() - today.getTime());
-    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)) - 1;
-    setLeftDays(diffDays);
+    if (eventDate < today) {
+      setLeftDays(-1);
+      return;
+    } else {
+      const diffTime = Math.abs(eventDate.getTime() - today.getTime());
+      const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)) - 1;
+      setLeftDays(diffDays);
+    }
   };
 
   useEffect(() => {
-    if (showInfo) {
-      setTimeout(() => setShowInfo(!showInfo), 3000);
-    }
+    // Suspense? Loading?
+    setLoading(true);
     calculateLeftDays();
-  }, [showInfo]);
+    setLoading(false);
+  }, []);
 
   return (
     <div className="relative mt-10 rounded-lg bg-violet-50 p-12">
@@ -48,21 +52,40 @@ const Event = () => {
         </div>
       )}
 
+      {/* <Suspense fallback={<div>Loading repo...</div>}> */}
+
       <div className="mt-4 text-center">
-        {leftDays <= 0 ? (
-          <span className="text-xl">It's Today!</span>
+        {loading ? (
+          <span className="text-xl">Loading ...</span>
+        ) : leftDays == 0 ? (
+          <span className="text-center text-xl">
+            It's Today! <span className="text-2xl">🎉</span>
+          </span>
+        ) : leftDays <= -1 ? (
+          <span className="text-center text-xl">
+            Passed
+          </span>
         ) : leftDays >= 2 ? (
           <div>
-            <span className="text-2xl">{leftDays}</span>
+            <span
+              className={`text-3xl ${leftDays <= 3 ? "text-red-500" : null}`}
+            >
+              {leftDays}
+            </span>
             <span className="text-xl"> days left</span>
           </div>
         ) : (
           <div>
-            <span className="text-2xl">{leftDays}</span>
+            <span
+              className={`text-3xl ${leftDays <= 3 ? "text-red-500" : null}`}
+            >
+              {leftDays}
+            </span>
             <span className="text-xl"> day left</span>
           </div>
         )}
       </div>
+      {/* </Suspense> */}
       <div className="mt-4 text-right">
         <span>
           {builtInEvents[0].date.year}/{builtInEvents[0].date.month}/
--- public/CONSTANTS.js
+++ public/CONSTANTS.js
@@ -1,26 +1,28 @@
 export const builtInQuotes = [
   {
-    quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
-    person: 'Nelson Mandela'
-  }, 
-  {
-    quote: "The way to get started is to quit talking and begin doing.",
-    person: 'Walt Disney'
+    quote:
+      "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+    person: "Nelson Mandela",
   },
   {
-    quote: "If life were predictable it would cease to be life, and be without flavor.",
-    person: 'Eleanor Roosevelt'
-  }
-]
+    quote: "The way to get started is to quit talking and begin doing.",
+    person: "Walt Disney",
+  },
+  {
+    quote:
+      "If life were predictable it would cease to be life, and be without flavor.",
+    person: "Eleanor Roosevelt",
+  },
+];
 
 export const builtInEvents = [
   {
     title: "Birthday",
     date: {
       year: 2023,
-      month: 5,
+      month: 7,
       day: 30,
     },
     description: "This is a birthday event",
-  }
-]
+  },
+];
