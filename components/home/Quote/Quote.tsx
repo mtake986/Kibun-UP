cb905033e4e302cf61a2a3194b6115eaf0c32417
mtake986 <tosa9629@gmail.com>
2023-06-02 17:34:58 -0600

    skelton

--- components/home/Quote/Quote.tsx
+++ components/home/Quote/Quote.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { builtInQuotes } from "../../../public/CONSTANTS";
 import { collection, onSnapshot } from "firebase/firestore";
 import { db } from "@/app/config/Firebase";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const Quote = () => {
   const [todaysQuote, setTodaysQuote] = useState<any>();
@@ -25,20 +26,21 @@ const Quote = () => {
 
   if (loading) {
     return (
-      <div className="mt-6 p-12">
-        <strong className="text-xl">Loading...</strong>
+      <div className="mt-6 p-12 flex-col items-center">
+        <Skeleton className="w-full h-32" />
+        <span className="flex justify-end mt-4 ">
+          <Skeleton className="h-7 w-32" />
+        </span>
       </div>
     );
   }
 
   return (
     <div className="mt-6 p-12">
-      <strong className="text-xl">
-        {loading ? "loading..." : todaysQuote ? todaysQuote.quote : null}
-      </strong>
+      <strong className="text-xl">{todaysQuote.quote}</strong>
       <div className="mt-4 text-right">
         <span>
-          - {loading ? "loading..." : todaysQuote ? todaysQuote.person : null}
+          - {todaysQuote.person}
         </span>
       </div>
     </div>
