3984e94b64b6ffdc0c7fb8f603f813257cbf109b
mtake986 <tosa9629@gmail.com>
2023-06-03 01:57:56 -0600

    reset. wrte data

--- components/event/register/Register.tsx
+++ components/event/register/Register.tsx
@@ -25,9 +25,15 @@ import { cn } from "@/lib/utils";
 import { format } from "date-fns";
 
 import { CalendarIcon } from "lucide-react";
+import { Textarea } from "@/components/ui/textarea";
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
+import { toast } from "@/components/ui/use-toast";
+import { auth, db } from "@/app/config/Firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const formSchema = z.object({
-  username: z.string().min(2).max(50),
+  eventTitle: z.string().min(2).max(50),
+  description: z.string().min(2).max(250),
   eventDate: z.date({
     required_error: "Please select a date and time",
     invalid_type_error: "That's not a date!",
@@ -35,86 +41,127 @@ const formSchema = z.object({
 });
 
 export default function Register() {
+  const [user] = useAuthState(auth);
+  const {reset } = useForm();
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      eventTitle: "",
+      description: "",
       eventDate: new Date(),
     },
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  async function onSubmit(values: z.infer<typeof formSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log(values);
+    // Add a new document with a generated id.
+    const collectionRef = collection(db, "events");
+    const docRef = await addDoc(collectionRef, {
+      ...values,
+      uid: user ? user.uid : "undefined",
+      createdAt: serverTimestamp(),
+    }).then(() => {
+      toast({
+        className: "border-none bg-green-500 text-white",
+        title: "Successfully Created",
+        description: `
+            Event Title: ${values.eventTitle}, 
+            Description: ${values.description},
+            Event Date: ${values.eventDate.toLocaleDateString("en-US")},
+          `,
+      });
+      reset({ eventTitle: "", description: "", eventDate: new Date()});
+      form.reset();
+    });
   }
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <div className="flex flex-col gap-5 sm:flex-row">
+          <FormField
+            control={form.control}
+            name="eventTitle"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormLabel>Event Title</FormLabel>
+                <FormControl>
+                  <Input placeholder="My Birthday" {...field} />
+                </FormControl>
+                {/* <FormDescription>
+                What event is coming up?
+              </FormDescription> */}
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="eventDate"
+            render={({ field }) => (
+              <FormItem className=" flex flex-col justify-between">
+                <FormLabel>Event Date</FormLabel>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button
+                        variant={"outline"}
+                        className={cn(
+                          "w-[240px] pl-3 text-left font-normal",
+                          !field.value && "text-muted-foreground"
+                        )}
+                      >
+                        {field.value ? (
+                          format(field.value, "PPP")
+                        ) : (
+                          <span>Pick a date</span>
+                        )}
+                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0" align="start">
+                    <Calendar
+                      mode="single"
+                      selected={field.value}
+                      onSelect={field.onChange}
+                      disabled={(date) => date < new Date()}
+                      initialFocus
+                    />
+                  </PopoverContent>
+                </Popover>
+                {/* <FormDescription>
+                Your date of birth is used to calculate your age.
+              </FormDescription> */}
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
         <FormField
           control={form.control}
-          name="username"
+          name="description"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Username</FormLabel>
+              <FormLabel>Description</FormLabel>
               <FormControl>
-                <Input placeholder="shadcn" {...field} />
+                <Textarea
+                  placeholder="My 23rd Birthday at my Parent's house"
+                  {...field}
+                />
               </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
 
-        <FormField
-          control={form.control}
-          name="eventDate"
-          render={({ field }) => (
-            <FormItem className="flex flex-col">
-              <FormLabel>Date of birth</FormLabel>
-              <Popover>
-                <PopoverTrigger asChild>
-                  <FormControl>
-                    <Button
-                      variant={"outline"}
-                      className={cn(
-                        "w-[240px] pl-3 text-left font-normal",
-                        !field.value && "text-muted-foreground"
-                      )}
-                    >
-                      {field.value ? (
-                        format(field.value, "PPP")
-                      ) : (
-                        <span>Pick a date</span>
-                      )}
-                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                    </Button>
-                  </FormControl>
-                </PopoverTrigger>
-                <PopoverContent className="w-auto p-0" align="start">
-                  <Calendar
-                    mode="single"
-                    selected={field.value}
-                    onSelect={field.onChange}
-                    disabled={(date) =>
-                      date < new Date()
-                    }
-                    initialFocus
-                  />
-                </PopoverContent>
-              </Popover>
-              <FormDescription>
-                Your date of birth is used to calculate your age.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
+        <Button type="submit">
+          Submit
+        </Button>
       </form>
     </Form>
   );
