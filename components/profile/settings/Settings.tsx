29c20d40eaf7cd3e4ba01096df3f3afae93d0090
mtake986 <tosa9629@gmail.com>
2023-10-06 19:47:18 -0600

    Add a label for the radios on Profile page

--- components/profile/settings/Settings.tsx
+++ components/profile/settings/Settings.tsx
@@ -1,16 +1,20 @@
 import HeadingThree from "@/components/utils/HeadingThree";
 import React from "react";
 import Radios from "./Radios";
+import RadioTitle from "./RadioTitle";
 
 const Settings = () => {
   return (
     <div className="mt-10">
-      <div className="flex items-center gap-5 sm:gap-10 justify-center">
-        <div className="sm:w-20 w-10 border border-b-[1px] border-black"></div>
+      <div className="flex items-center justify-center gap-5 sm:gap-10">
+        <div className="w-10 border border-b-[1px] border-black sm:w-20"></div>
         <HeadingThree text="Settings" />
-        <div className="sm:w-20 w-10 border border-b-[1px] border-black"></div>
+        <div className="w-10 border border-b-[1px] border-black sm:w-20"></div>
       </div>
-      <Radios />
+      <div>
+        <RadioTitle />
+        <Radios />
+      </div>
     </div>
   );
 };
