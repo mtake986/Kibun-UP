20a65a1a9ed3f5565127084371bca89c2c490fb6
mtake986 <tosa9629@gmail.com>
2023-10-24 07:28:11 -0600

    Select design

--- components/ui/select.tsx
+++ components/ui/select.tsx
@@ -19,7 +19,7 @@ const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      "bg-slate-50 dark:bg-slate-800 border-input ring-offset-background placeholder:text-muted-foreground focus:ring-ring flex h-10 w-full items-center justify-between rounded-md border bg-transparent px-3 py-2 text-sm ring-0 ring-offset-0 duration-300 hover:opacity-70  focus:outline-none focus:ring-0 focus:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 dark:border-none min-w-[8rem]",
+      "border-input ring-offset-background placeholder:text-muted-foreground focus:ring-ring flex h-10 w-full min-w-[8rem] items-center justify-between rounded-md border bg-slate-50 bg-transparent px-3 py-2 text-sm ring-0 ring-offset-0 duration-300 hover:opacity-70  focus:outline-none focus:ring-0 focus:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 dark:border-none dark:bg-slate-800",
       className
     )}
     {...props}
@@ -40,7 +40,7 @@ const SelectContent = React.forwardRef<
     <SelectPrimitive.Content
       ref={ref}
       className={cn(
-        "bg-white dark:bg-slate-900 bg-popover text-popover-foreground animate-in fade-in-80 relative z-50 min-w-[8rem] overflow-hidden rounded-md border text-xs shadow-md dark:border-none dark:text-white",
+        "bg-popover text-popover-foreground animate-in fade-in-80 dark:borde-none relative z-50 min-w-[8rem] overflow-hidden rounded-md border bg-white text-xs shadow-sm dark:shadow-white dark:border-none dark:bg-slate-800 dark:text-white",
         position === "popper" && "translate-y-1",
         className
       )}
