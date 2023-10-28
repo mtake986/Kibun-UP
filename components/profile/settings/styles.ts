775e918be21b91c57d2d9fb0507a3b83ab74b994
mtake986 <tosa9629@gmail.com>
2023-10-28 11:14:34 -0600

    add input property in styleVarables

--- components/profile/settings/styles.ts
+++ components/profile/settings/styles.ts
@@ -4,4 +4,5 @@ export const styleVariables = {
     chosen:
       "rounded-md border border-violet-500 p-3 text-violet-500 sm:p-5 bg-violet-50 dark:bg-slate-950 text-violet-500",
   },
+  input: 'border-gray-300 text-violet-600 dark:border-violet-500'
 };
