3c79cda59e0f6701ead64ef73bc3cf488bd8352b
mtake986 <tosa9629@email.com>
2023-12-03 10:54:19 -0700

    remove transition.tsx from UserActivityPage

--- app/user/profile/[uid]/activity/page.tsx
+++ app/user/profile/[uid]/activity/page.tsx
@@ -1,16 +1,11 @@
 "use client";
-type IndexPageProps = {};
-type IndexPageRef = React.ForwardedRef<HTMLDivElement>;
 
 import UserActivity from "@/components/profile/activity/UserActivity";
 import React from "react";
-import PageTransition from "@/app/transition";
 
-const UserActivityPage = (props: IndexPageProps, ref: IndexPageRef) => {
+const UserActivityPage = () => {
   return (
-    <PageTransition ref={ref}>
       <UserActivity />
-    </PageTransition>
   );
 };
 
