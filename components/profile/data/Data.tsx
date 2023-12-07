fa5a3cad583b2d49552ec63c1ede1f07599aaff9
mtake986 <tosa9629@email.com>
2023-12-06 23:32:45 -0700

    remove unnecessary import

--- components/profile/data/Data.tsx
+++ components/profile/data/Data.tsx
@@ -1,8 +1,6 @@
-import { useQuote } from "@/context/QuoteContext";
 import QuoteList from "./tabs/quotes/QuoteList";
 import EventList from "./tabs/events/EventList";
 import MobileSortFilterForQuotesOpenBtn from "./tabs/quotes/MobileSortFilterForQuotesOpenBtn";
-import { TypeLoginUser } from "@/types/type";
 import Tabs from "./tabs/Tabs";
 import SectionTitle from "../SectionTitle";
 import { useSearchParams } from "next/navigation";
