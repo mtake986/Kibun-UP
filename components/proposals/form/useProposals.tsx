3d113214517ee3e287e4a497ff0221545b40657f
mtake986 <tosa9629@email.com>
2023-12-27 16:56:59 -0700

    add a catch

--- components/proposals/form/useProposals.tsx
+++ components/proposals/form/useProposals.tsx
@@ -1,25 +1,34 @@
 import { db } from "@/config/Firebase";
-import { displaySuccessToast } from "@/functions/displayToast";
+import { displayErrorToast, displaySuccessToast } from "@/functions/displayToast";
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
-import React from 'react'
+import React from "react";
 
 const useProposals = () => {
   const proposalsCollectionRef = collection(db, "proposals");
 
-  const submitProposal = async (values: {title: string, detail: string}, uid: string) => {
+  const submitProposal = async (
+    values: { title: string; detail: string },
+    uid: string
+  ) => {
     const currTime = serverTimestamp();
     await addDoc(proposalsCollectionRef, {
       ...values,
       createdBy: uid,
       votedUpBy: [],
       createdAt: currTime,
-    }).then(() => {
-      displaySuccessToast({
-        text: "Success: Created",
+    })
+      .then(() => {
+        displaySuccessToast({
+          text: "Success: Created",
+        });
+      })
+      .catch((error) => {
+        displayErrorToast({
+          text: "Error: " + error.message,
+        });
       });
-    });
-  }
-  return {submitProposal};
-}
+  };
+  return { submitProposal };
+};
 
-export default useProposals
+export default useProposals;
