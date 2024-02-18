b38d1fa905ef102d9fc692a5712e7890d60e9e9a
mtake986 <tosa9629@email.com>
2024-02-18 14:08:12 -0700

    hover card to popover

--- components/home/event/DescriptionToggleBtn.tsx
+++ components/home/event/DescriptionToggleBtn.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import {
-  HoverCard,
-  HoverCardContent,
-  HoverCardTrigger,
-} from "@/components/ui/hover-card";
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import { TypeEvent } from "@/types/type";
 import { AiOutlineInfoCircle } from "react-icons/ai";
 type Props = {
@@ -12,12 +12,12 @@ type Props = {
 const DescriptionToggleBtn = ({ event }: Props) => {
   if (event.description) {
     return (
-      <HoverCard>
-        <HoverCardTrigger className="absolute right-5 top-5 cursor-pointer duration-300 hover:opacity-50 sm:right-12 sm:top-12">
+      <Popover>
+        <PopoverTrigger className="absolute right-5 top-5 cursor-pointer duration-300 hover:opacity-50 sm:right-12 sm:top-12">
           <AiOutlineInfoCircle size={14} />
-        </HoverCardTrigger>
-        <HoverCardContent>{event.description}</HoverCardContent>
-      </HoverCard>
+        </PopoverTrigger>
+        <PopoverContent className="text-xs bg-white dark:bg-slate-800">{event.description}</PopoverContent>
+      </Popover>
     );
   } else return null;
 };
