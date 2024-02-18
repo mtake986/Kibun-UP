2b20d2ee6efb70b704e031710ef3785cc0f8000b
mtake986 <tosa9629@email.com>
2024-02-18 14:13:45 -0700

    code rabbit

--- components/home/event/DescriptionToggleBtn.tsx
+++ components/home/event/DescriptionToggleBtn.tsx
@@ -13,10 +13,16 @@ const DescriptionToggleBtn = ({ event }: Props) => {
   if (event.description) {
     return (
       <Popover>
-        <PopoverTrigger className="absolute right-5 top-5 cursor-pointer duration-300 hover:opacity-50 sm:right-12 sm:top-12">
+        <PopoverTrigger
+          className="absolute right-5 top-5 cursor-pointer duration-300 hover:opacity-50 sm:right-12 sm:top-12"
+          aria-label="Display a description of the event"
+          role="button"
+        >
           <AiOutlineInfoCircle size={14} />
         </PopoverTrigger>
-        <PopoverContent className="text-xs bg-white dark:bg-slate-800">{event.description}</PopoverContent>
+        <PopoverContent className="bg-white text-xs dark:bg-slate-800">
+          {event.description}
+        </PopoverContent>
       </Popover>
     );
   } else return null;
